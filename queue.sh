67e0d99ce8b81576765125e455e59a278f5d84ab
Rudie Dirkx <github@hotblocks.nl>
2016-11-12 14:05:55 +0100

    forever queue

--- queue.sh
+++ queue.sh
@@ -1,18 +1,21 @@
-# Read line 1
-line=$(head -n1 ./.queue)
-echo $line
-
 # Start line loop
-while [ "$line" != "" ]; do
+#while [ "$line" != "" ]; do
+while true; do
 
-	# Remove line 1
-	sed -i '1d' ./.queue
-
-	# Call the download script
-	/usr/bin/env php download.php $line
-
-	# Read next line 1
+	# Read line 1
 	line=$(head -n1 ./.queue)
 	echo $line
+
+	if [ "$line" != "" ]; then
+		# Remove line 1
+		sed -i '1d' ./.queue
+
+		# Call the download script
+		/usr/bin/env php download.php $line
+	else
+		echo "Queue empty"
+	fi
+
+	sleep 2
 
 done
